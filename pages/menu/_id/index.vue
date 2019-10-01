40a34054e1ff161bc076c13a6a3761c17798f26b
Antoine Parat <paratantoine@gmail.com>
2019-10-01 20:20:46 +0200

    test now

---
+++ pages/menu/_id/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>name : {{name}}</p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            name : this.$route.params.id
+        }
+    },
+    head () {
+    return {
+      title: this.name,
+      meta: [
+        // hid est utiliser comme identifiant unique. N'utilisez pas `vmid` car ça ne fonctionnera pas
+        { hid: 'description', name: this.name, content: `Ma description de ${this.name}` }
+      ]
+    }
+  }
+}
+</script>
